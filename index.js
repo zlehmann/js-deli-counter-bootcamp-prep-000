387d4acde17acb908d3b90a4aa61da272f16da40
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 18:40:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   } else {
     for (let i=0; i<katzDeliLine.length; i++) {
-      output.push(` ${i+1}. ${katzDeliLine[i]}`)
+      output.push(`${i+1}. ${katzDeliLine[i]}`)
     }
     var str = output.toString()
     return (`The line is currently: ${str}`)
