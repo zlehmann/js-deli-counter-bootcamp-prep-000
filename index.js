748d5a30746f2cfb417d1c7fdea099740d85775a
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 17:52:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(katzDeliLine, currentName) {
   katzDeliLine.push(currentName)
-  return `Welcome ${currentName}. You are number ${katzDeliLine.length} in line.`
+  return `Welcome, ${currentName}. You are number ${katzDeliLine.length} in line.`
 }
 
