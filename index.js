8d53addeaa0b1202fa0381966f43e86a195f130e
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 17:43:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function takeANumber(currentLine, currentName) {
-  
+  for (let i=0; i<currentLine.length; i++) {
+    if (currentLine[i] == currentName) {
+      return `Welcome ${currentline[i]}. You are number ${i + 1} in line.`
+    }
+  }
+}
+
+function nowServing() {
+  return
 }
