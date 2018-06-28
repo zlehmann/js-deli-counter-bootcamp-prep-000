6d2843295a2b0b833f94a135090391c9e7d4b967
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 17:59:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function nowServing(katzDeliLine) {
   }
   return `Currently serving ${katzDeliLine.shift()}.`
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "The line is currently empty."
+  }
+}
