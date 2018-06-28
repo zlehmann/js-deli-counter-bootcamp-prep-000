bd3d001e46f239656025a72f96cb84b1683fc3fe
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 18:22:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function currentLine(katzDeliLine) {
   } else {
     var output = []
     for (let i=0; i<katzDeliLine.length; i++) {
-      output.concat(`${i+1}. `)
-      output.concat(katzDeliLine[i])
+      output.concat(`${i+1}. ${katzDeliLine[i]}`)
     }
   return (`The line is currently: ${output}`)
   }
