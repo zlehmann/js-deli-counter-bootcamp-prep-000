5a449db079c90af1f2abd1c17d2a2e680e1e68e0
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 17:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 function takeANumber(katzDeliLine, currentName) {
-  for (let i=0; i<currentLine.length; i++) {
-    if (currentLine[i] == currentName) {
-      return `Welcome ${currentline[i]}. You are number ${i + 1} in line.`
-    }
-  }
-}
-
-function nowServing() {
-  if ()
-}
-
-function currentLine() {
-  for (let i=0; i<katzDeliLine.length; i++) {
-    
-  }
+  katzDeliLine.push(currentName)
+  
 }
