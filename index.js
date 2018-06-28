ebc11a7c79b6882f5eefd82abf163c280fef8020
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 18:03:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   } else {
     var output = []
-    for (let i=0; i<katzDeliLine.length) {
+    for (let i=0; i<katzDeliLine.length; i++) {
       output.concat(`${i+1}. `)
       output.concat(katzDeliLine[i])
     }
