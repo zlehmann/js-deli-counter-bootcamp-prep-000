d8e05172b5648ca05933dc32f686d1c3bd9a5ba3
zlehmann@gmail.com <zlehmann@gmail.com>
2018-06-28 17:53:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber(katzDeliLine, currentName) {
   return `Welcome, ${currentName}. You are number ${katzDeliLine.length} in line.`
 }
 
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length == 0) {
+    return "There is nobody waiting to be served!"
+  }
+  return
+}
